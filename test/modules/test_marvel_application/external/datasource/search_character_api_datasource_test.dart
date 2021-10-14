5389322ae555cd02641109af89a43c695dcc51a2
Lucas Sousa <lucasdejesussousa159@gmail.com>
2021-10-14 20:38:40 -0300

    Finish Test

--- test/modules/test_marvel_application/external/datasource/search_character_api_datasource_test.dart
+++ test/modules/test_marvel_application/external/datasource/search_character_api_datasource_test.dart
@@ -1,22 +1,16 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
 import 'package:test_marvel_application/modules/test_marvel_application/external/datasource/search_character_api_datasource.dart';
-
-class DioMock extends Mock implements Dio {}
 
 main() {
   final dio = Dio();
 
   final datasource = SearchCharacterApiDatasource(dio);
-  
-  test("Must return a ContentCharacter when request success complete", () async {
 
-    // when(dio.get(any)).thenAnswer((_) async => Response(data: jsonDecode(resultResponse), statusCode: 200, requestOptions: RequestOptions(path: '')));
-
+  test("Must return a ContentCharacter when request success complete",
+      () async {
     final future = datasource.searchCharacter("Iron");
 
     expect(future, completes);
-
   });
 }
